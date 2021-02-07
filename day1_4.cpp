6e1f9e497ff6126276a82a77289863f6558e622f
Jayesh Gaikwad <78681221+jayeshdg@users.noreply.github.com>
2021-02-07 16:29:38 +0530

    Add files via upload

---
+++ day1_4.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+    int num1;
+    int num2;
+    cout<<"Enter Num1 :";
+    cin>>num1;
+    cout<<"Enter Num2 :";
+    cin>>num2;
+
+    cout<<"Addition "<<num1+num2;
+    return 0;
+    
+}
+
+/*
+#include<iostream>
+
+
+int main(void)
+{
+    int num1;
+    int num2;
+    std::cout<<"Enter Num1 :";
+    std::cin>>num1;
+    std::cout<<"Enter Num2 :";
+    std::cin>>num2;
+
+    std::cout<<"Addition "<<num1+num2;
+    return 0;
+    
+}
+
+*/
